4ea86f3287ffb43ddca484505a66d2fd074bc5de
seeke <seekeryang7@gmail.com>
2020-08-01 17:48:13 +0900

    network

--- projecta/board/views.py
+++ projecta/board/views.py
@@ -5,70 +5,88 @@ import datetime
 # Create your views here.
 def index (request):
     return render(request, 'index.html')
+
+def about (request):
+    return render(request, 'about.html')
     
-def general(request):
-    articles = Article.objects.all()
-    articles_number = articles.count()
-    return render(request, 'general.html', {
-        'general': articles,
-        'articles_number' : articles_number
-        })
+def curriculum (request):
+    return render(request, 'curriculum.html')
 
-def notice(request):
-    announcements = Announcement.objects.all()
-    announcements_number = announcements.count()
-    return render(request, 'notice.html', {
-        'notice': announcements,
-        'announcements_number' : announcements_number
-        })
+def network (request):
+    return render(request, 'network.html')
 
-def qa(request):
-    questions = Question.objects.all()
-    questions_number = questions.count()
-    return render(request, 'qa.html', {
-        'qa': questions,
-        'questions_number' : questions_number
-        })
+def network_detail (request):
+    return render(request, 'network_detail.html')
 
-def detail_general(request, pk_selected):
-    article = Article.objects.get(pk=pk_selected)
-    return render(request, 'detail_general.html', {'detail_general': article})
+def board (request):
+    return render(request, 'board.html')  
 
-def detail_notice(request, pk_selected):
-    announcement = Announcement.objects.get(pk=pk_selected)
-    return render(request, 'detail_notice.html', {'detail_notice': announcement})
+def contact (request):
+    return render(request, 'contact.html') 
+    
+# def general(request):
+#     articles = Article.objects.all()
+#     articles_number = articles.count()
+#     return render(request, 'general.html', {
+#         'general': articles,
+#         'articles_number' : articles_number
+#         })
 
-def detail_qa(request, pk_selected):
-    question = Question.objects.get(pk=pk_selected)
-    return render(request, 'detail_qa.html', {'detail_qa': question})
+# def notice(request):
+#     announcements = Announcement.objects.all()
+#     announcements_number = announcements.count()
+#     return render(request, 'notice.html', {
+#         'notice': announcements,
+#         'announcements_number' : announcements_number
+#         })
 
-def write_general(request):
-    if request.method == 'POST':
-        new_article = Article.objects.create(
-            title = request.POST['title'],
-            content = request.POST['content'],
-            time = datetime.datetime.now()
-        )
-        return redirect('detail_general', pk_selected = new_article.pk)
-    return render(request, 'write_general.html')
+# def qa(request):
+#     questions = Question.objects.all()
+#     questions_number = questions.count()
+#     return render(request, 'qa.html', {
+#         'qa': questions,
+#         'questions_number' : questions_number
+#         })
 
-def write_notice(request):
-    if request.method == 'POST':
-        new_announcement = Announcement.objects.create(
-            title = request.POST['title'],
-            content = request.POST['content']
-        )
-        return redirect('detail_notice', pk_selected = new_announcement.pk)
-    return render(request, 'write_notice.html')
+# def detail_general(request, pk_selected):
+#     article = Article.objects.get(pk=pk_selected)
+#     return render(request, 'detail_general.html', {'detail_general': article})
 
-def write_qa(request):
-    if request.method == 'POST':
-        new_question = Question.objects.create(
-            title = request.POST['title'],
-            content = request.POST['content']
-        )
-        return redirect('detail_qa', pk_selected = new_question.pk)
-    return render(request, 'write_qa.html')
+# def detail_notice(request, pk_selected):
+#     announcement = Announcement.objects.get(pk=pk_selected)
+#     return render(request, 'detail_notice.html', {'detail_notice': announcement})
+
+# def detail_qa(request, pk_selected):
+#     question = Question.objects.get(pk=pk_selected)
+#     return render(request, 'detail_qa.html', {'detail_qa': question})
+
+# def write_general(request):
+#     if request.method == 'POST':
+#         new_article = Article.objects.create(
+#             title = request.POST['title'],
+#             content = request.POST['content'],
+#             time = datetime.datetime.now()
+#         )
+#         return redirect('detail_general', pk_selected = new_article.pk)
+#     return render(request, 'write_general.html')
+
+# def write_notice(request):
+#     if request.method == 'POST':
+#         new_announcement = Announcement.objects.create(
+#             title = request.POST['title'],
+#             content = request.POST['content']
+#         )
+#         return redirect('detail_notice', pk_selected = new_announcement.pk)
+#     return render(request, 'write_notice.html')
+
+# def write_qa(request):
+#     if request.method == 'POST':
+#         new_question = Question.objects.create(
+#             title = request.POST['title'],
+#             content = request.POST['content']
+#         )
+#         return redirect('detail_qa', pk_selected = new_question.pk)
+#     return render(request, 'write_qa.html')
 
 
 
